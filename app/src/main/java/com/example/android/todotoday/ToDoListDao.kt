033f7c098e2e72e21b6eafeae7861bddc309cb77
ioana <>
2018-10-26 14:46:59 +0100

    wip

---
+++ app/src/main/java/com/example/android/todotoday/ToDoListDao.kt
@@ -0,0 +1,11 @@
+package com.example.android.todotoday
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Query
+
+@Dao
+interface ToDoListDao {
+
+    @Query ("SELECT * FROM ")
+
+}
